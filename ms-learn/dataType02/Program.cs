0df4ca103d4309465403f099d0903b9f0e351c9d
CHENGJUHO <chengjuho@naver.com>
2026-05-28 00:52:32 +0900

    feat: 자료형·튜플 실습 추가

---
+++ ms-learn/dataType02/Program.cs
@@ -0,0 +1,72 @@
+﻿// WorkWithIntegers();
+OrderPrecedence();
+
+void WorkWithIntegers()
+{
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+    Console.WriteLine(c);
+
+
+    // subtraction
+    c = a - b;
+    Console.WriteLine(c);
+
+    // multiplication
+    c = a * b;
+    Console.WriteLine(c);
+
+    // division
+    c = a / b;
+    Console.WriteLine(c);
+}
+
+void OrderPrecedence()
+{
+    int a = 5;
+    int b = 4;
+    int c = 2;
+    int d = a + b * c;
+    Console.WriteLine(d);
+
+    d = (a + b) * c;
+    Console.WriteLine(d);
+
+    d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+    Console.WriteLine(d);
+
+    int e = 7;
+    int f = 4;
+    int g = 3;
+    int h = (e + f) / g;
+    Console.WriteLine(h);
+}
+Console.WriteLine("==");
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}");
+
+Console.WriteLine("==");
+double max1 = double.MaxValue;
+double min1 = double.MinValue;
+Console.WriteLine($"The range of double is {min1} to {max1}");
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
+
+Console.WriteLine("==");
+//decimal
+decimal min2 = decimal.MinValue;
+decimal max2 = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {min2} to {max2}");
+
+double a = 1.0;
+double b = 3.0;
+Console.WriteLine(a / b);
+
+decimal c = 1.0M;
+decimal d = 3.0M;
+Console.WriteLine(c / d);
---
+++ ms-learn/tuples03/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace tuples03;
+
+class Program
+{
+    public record Point(int X, int Y);
+    static void Main(string[] args)
+    {
+        // 튜플은 고정 길이를 가진 정렬된 값 시퀀스입니다. 튜플의 각 요소에는 형식과 선택적 이름이 있습니다.
+        var pt = (X: 1, Y: 2);
+
+        var slope = (double)pt.Y / (double)pt.X;
+        Console.WriteLine($"A line from the origin to the point {pt} has a slope of {slope}.");
+
+        pt.X = pt.X + 5;
+        Console.WriteLine($"The point is now at {pt}.");
+
+        Console.WriteLine("==");
+        var subscript = (A: 0, B: 0);
+        subscript = pt;
+        Console.WriteLine(subscript);
+
+        Console.WriteLine("==");
+        var namedData = (Name: "Morning observation", Temp: 17, Wind: 4);
+        Console.WriteLine(namedData);
+        // 튜플 형식에는 이름이 없으므로 값 집합에 의미를 전달할 수 없습니다. 튜플 형식은 동작을 추가할 수 없습니다.
+        var person = (FirstName: "", LastName: "");
+        var order = (Product: "guitar picks", style: "triangle", quantity: 500, UnitPrice: 0.10m);
+
+        Console.WriteLine("==");
+        Point pt3 = new Point(1, 1);
+        var pt4 = pt3 with { Y = 10 };
+        Console.WriteLine($"The two points are {pt3} and {pt4}");
+        
+    }
+}
