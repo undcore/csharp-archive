83bddcb28a5609d11083009949fa49cc6496f705
CHENGJUHO <chengjuho@naver.com>
2026-05-29 18:06:15 +0900

    feat: MS Learn 실습 정리 (분기·반복문)

--- ms-learn/03_tuples/Program.cs
+++ ms-learn/03_tuples/Program.cs
@@ -1,8 +1,15 @@
-﻿namespace tuples03;
+﻿/*
+# 튜플은 복잡한 LINQ 표현식에서 유용해. 예전에는 익명 타입을 사용했지 (그리고 IQueryable에서는 LINQ 제공자가 표현식 트리를 검사할 때 그걸 기대하기 때문에 보통 그렇게 해야 해) 근데 ValueTuple을 사용하면 "루프 안에서" 힙 할당을 피할 수 있어.
+# 여러 값을 반환하기 위해서만 존재하는 일회용 클래스의 확산을 줄여줍니다. 이는 더 큰 프로젝트에서 각 클래스에 대한 수많은 개별 파일을 생성하고 IntelliSense 및 유사한 시나리오에서 혼잡을 야기하여 상당한 양의 노이즈를 유발할 수 있습니다. 또한 패턴 매칭 에서도 유용하며, 많은 경우 익명 클래스의 대안으로 사용됩니다.
+*/
+namespace tuples03;
 
 class Program
 {
-    public record Point(int X, int Y);
+    public record Point(int X, int Y){
+        public double Slope() => (double)Y / (double)X;
+    }
+    public record struct Point2(int X, int Y);
     static void Main(string[] args)
     {
         // 튜플은 고정 길이를 가진 정렬된 값 시퀀스입니다. 튜플의 각 요소에는 형식과 선택적 이름이 있습니다.
@@ -31,5 +38,12 @@ class Program
         var pt4 = pt3 with { Y = 10 };
         Console.WriteLine($"The two points are {pt3} and {pt4}");
         
+        Console.WriteLine("==");
+        double slopeResult = pt4.Slope();
+        Console.WriteLine($"The slope of {pt4} is {slopeResult}");
     }
 }
+/*
+class - reference type
+struct - value type
+*/
---
+++ ms-learn/04_branches_loops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+//
+int a = 5;
+int b = 6;
+b = 3;
+if (a + b > 10){
+    Console.WriteLine("The answer is greater than 10.");
+    int c = a+b;
+    Console.WriteLine($"answer = {c}");
+    }
+else
+{
+    System.Console.WriteLine("The answer is not greater than 10");
+
+}
+//
+System.Console.WriteLine("");
+int a1= 5;
+int b1 = 3;
+int c1 = 4;
+if ((a1 + b1 + c1 > 10) && (a == b))
+{
+    Console.WriteLine("The answer is greater than 10");
+    Console.WriteLine("And the first number is equal to the second");
+}
+else
+{
+    Console.WriteLine("The answer is not greater than 10");
+    Console.WriteLine("Or the first number is not equal to the second");
+}
+//
+System.Console.WriteLine("");
